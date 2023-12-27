0d49beee06c605072e8acb8ce9ea5eeeec559086
DodleGek <2982780802@qq.com>
2023-12-27 10:11:53 +0800

    处理业务逻辑

--- src/main/java/org/example/article/service/CommentService.java
+++ src/main/java/org/example/article/service/CommentService.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Service
 public class CommentService {
-    //注入dao
+    // 通过Spring的依赖注入机制，自动创建并注入CommentRepository和MongoTemplate实例
     private final CommentRepository commentRepository;
     private final MongoTemplate mongoTemplate;
 
@@ -26,74 +26,80 @@ public class CommentService {
     }
 
     /**
-     * 保存一个评论
+     * 保存评论
      *
-     * @param comment 要保存的评论内容
+     * @param comment 要保存的评论对象
      */
     public void saveComment(Comment comment) {
-        //如果需要自定义主键，可以在这里指定主键，否则会自动生成主键
-        //设置一些默认初始值
-        //调用dao
+        // 如果Comment对象的ID已存在，则更新记录；否则，创建新记录
         commentRepository.save(comment);
     }
 
     /**
      * 更新评论
      *
-     * @param comment 要更新的评论内容
+     * @param comment 要更新的评论对象
      */
     public void updateComment(Comment comment) {
-        //调用dao
+        // 保存或更新评论
         commentRepository.save(comment);
     }
 
     /**
-     * 根据id删除评论
+     * 根据ID删除评论
      *
-     * @param id 要删除的评论的id
+     * @param id 要删除的评论的ID
      */
     public void deleteCommentById(String id) {
-        //调用dao
+        // 删除指定ID的评论
         commentRepository.deleteById(id);
     }
 
     /**
      * 查询所有评论
      *
-     * @return List<Comment>
+     * @return 返回所有评论的列表
      */
     public List<Comment> findCommentList() {
-        //调用dao
+        // 查询所有评论
         return commentRepository.findAll();
     }
 
     /**
-     * 根据id查询评论
+     * 根据ID查询评论
      *
-     * @param id 要查询的评论的id
-     * @return Comment
+     * @param id 要查询的评论的ID
+     * @return 返回查询到的评论，如果没有找到则返回null
      */
     public Comment findCommentById(String id) {
-        //调用dao
+        // 查询指定ID的评论
         return commentRepository.findById(id).orElse(null);
     }
 
+    /**
+     * 根据父评论的ID查询子评论，并进行分页处理
+     *
+     * @param parentid 父评论的ID
+     * @param page 当前页码
+     * @param size 每页显示的评论数量
+     * @return 返回一个Page对象，包含查询到的子评论列表和分页信息
+     */
     public Page<Comment> findCommentListBy(String parentid, int page, int size) {
-        //调用dao
+        // 根据父评论的ID查询子评论，并进行分页处理
         return commentRepository.findByParentid(parentid, PageRequest.of(page - 1, size));
     }
 
     /**
-     * 点赞数+1
-     * @param id 评论id
+     * 对指定评论的点赞数进行+1操作
+     * @param id 评论的ID
      */
     public void updateCommentLikenum(String id) {
-        //查询条件
+        // 构建查询条件
         Query query = Query.query(Criteria.where("_id").is(id));
-        //更新条件
+        // 构建更新条件，使用$inc操作符，表示要增加的字段
         Update update = new Update();
         update.inc("likenum");
+        // 更新第一个匹配的文档
         mongoTemplate.updateFirst(query, update, Comment.class);
     }
-
 }
