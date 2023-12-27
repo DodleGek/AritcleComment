edbcd74478bec00c95efbe591d77061819107ff3
DodleGek <2982780802@qq.com>
2023-12-27 10:20:59 +0800

    继承Spring Data JPA，定义CRUD操作

--- src/main/java/org/example/article/dao/CommentRepository.java
+++ src/main/java/org/example/article/dao/CommentRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
+    // 继承MongoRepository接口，MongoRepository是Spring Data MongoDB库提供的一个接口，用于操作MongoDB数据库
+    // Comment是实体类型，String是Comment实体的ID类型
+
+    // 根据父评论ID查询评论，并返回分页结果
+    // parentid是父评论的ID
+    // pageable是一个接口，封装了分页信息，包括页码和每页的记录数
     Page<Comment> findByParentid(String parentid, Pageable pageable);
 }
