b141b1cdcdd98ddc8a2c7a29dcc036addaa209f1
DodleGek <2982780802@qq.com>
2023-12-27 10:15:12 +0800

    使用lombok简化代码

--- src/main/java/org/example/article/po/Comment.java
+++ src/main/java/org/example/article/po/Comment.java
@@ -1,5 +1,8 @@
 package org.example.article.po;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,131 +13,38 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@Document(collection = "comment")//可以省略，如果省略，则默认使用类名小写映射集合
-//复合索引
-@CompoundIndex(def = "{'userid':1,'nickname':-1}")
+@Getter
+@Setter
+@ToString
+@Document(collection = "comment") // 如果省略，将默认使用类名小写映射集合
+@CompoundIndex(def = "{'userid':1,'nickname':-1}") // 创建复合索引，userid是升序，nickname是降序
 public class Comment implements Serializable {
 
     @Id
-    private String id;//主键
-    //该属性对应mongodb的字段的名字，如果一致，则无需该注解
+    private String id; // 主键ID
+
     @Field("content")
-    private String content;//吐槽内容
-    private Date publishtime;//发布日期
-    //添加一个单字段索引
+    private String content; // 评论内容，对应mongodb的字段名，如果一致，则无需该注解
+
+    private Date publishtime; // 评论发布日期
+
     @Indexed
-    private String userid;//发布人ID
-    private String nickname;//昵称
-    private LocalDateTime createdatetime;//评论的日期时间
-    private Integer likenum;//点赞数
-    private Integer replynum;//回复数
-    private String state;//状态
-    private String parentid;//上级ID
-    private String articleid;
+    private String userid; // 发布人ID，添加一个单字段索引
 
-    //getter and setter
-    public String getId() {
-        return id;
-    }
+    private String nickname; // 发布人昵称
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    private LocalDateTime createdatetime; // 评论创建的日期和时间
 
-    public String getContent() {
-        return content;
-    }
+    private Integer likenum; // 评论的点赞数
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private Integer replynum; // 评论的回复数
 
-    public Date getPublishtime() {
-        return publishtime;
-    }
+    private String state; // 评论的状态
 
-    public void setPublishtime(Date publishtime) {
-        this.publishtime = publishtime;
-    }
+    private String parentid; // 父评论ID
 
-    public String getUserid() {
-        return userid;
-    }
+    private String articleid; // 文章ID
 
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public LocalDateTime getCreatedatetime() {
-        return createdatetime;
-    }
-
-    public void setCreatedatetime(LocalDateTime createdatetime) {
-        this.createdatetime = createdatetime;
-    }
-
-    public Integer getLikenum() {
-        return likenum;
-    }
-
-    public void setLikenum(Integer likenum) {
-        this.likenum = likenum;
-    }
-
-    public Integer getReplynum() {
-        return replynum;
-    }
-
-    public void setReplynum(Integer replynum) {
-        this.replynum = replynum;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getParentid() {
-        return parentid;
-    }
-
-    public void setParentid(String parentid) {
-        this.parentid = parentid;
-    }
-
-    public String getArticleid() {
-        return articleid;
-    }
-
-    public void setArticleid(String articleid) {
-        this.articleid = articleid;
-    }
-
-    @Override
-    public String toString() {
-        return "comment{" +
-                "id='" + id + '\'' +
-                ", content='" + content + '\'' +
-                ", publishtime=" + publishtime +
-                ", userid='" + userid + '\'' +
-                ", nickname='" + nickname + '\'' +
-                ", createdatetime=" + createdatetime +
-                ", likenum=" + likenum +
-                ", replynum=" + replynum +
-                ", state='" + state + '\'' +
-                ", parentid='" + parentid + '\'' +
-                ", articleid='" + articleid + '\'' +
-                '}';
-    }
+    // getter和setter方法
+    // toString方法
 }
