c867b786e73eef1eba159f9d4775871b5f2ee178
DodleGek <2982780802@qq.com>
2023-12-27 11:36:25 +0800

    补充接口方法

--- src/main/java/org/example/article/dao/CommentRepository.java
+++ src/main/java/org/example/article/dao/CommentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends MongoRepository<Comment, String> {
     // 继承MongoRepository接口，MongoRepository是Spring Data MongoDB库提供的一个接口，用于操作MongoDB数据库
     // Comment是实体类型，String是Comment实体的ID类型
@@ -13,4 +15,7 @@ public interface CommentRepository extends MongoRepository<Comment, String> {
     // parentid是父评论的ID
     // pageable是一个接口，封装了分页信息，包括页码和每页的记录数
     Page<Comment> findByParentid(String parentid, Pageable pageable);
+
+    // 根据文章ID查询评论
+    List<Comment> findByArticleid(String articleId);
 }
